ddc510cf163b6f267158aea7e74b838fec199a55
rodolfowei <crm_b@hotmail.com>
2015-05-12 18:07:27 +0200

    Adding a display to sehow how many rows an colums have each of the images of the ArrayList<Mat>

--- src/comparador/Aplicacion.java
+++ src/comparador/Aplicacion.java
@@ -2,6 +2,9 @@ package comparador;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.opencv.core.Mat;
 
 //Change to test gitHub
 //Second change
@@ -14,11 +17,20 @@ public class Aplicacion {
 		
 		ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
 		images = Imagesfromfolder.getimages();
+		
+		ArrayList<Mat> imagesOcv = new ArrayList<Mat>();
+		imagesOcv = Imagesfromfolder.getimagesMat();
 
 		for(BufferedImage bufim : images)
 		{
 			System.out.println(bufim.getWidth());
 			System.out.println(bufim.getHeight());
+		}
+		
+		for (Mat matim : imagesOcv)
+		{
+			System.out.print(matim.cols());
+			System.out.println(matim.rows());
 		}
 	}
 

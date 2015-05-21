9b18c2f96927fc0e90d16fbc54e2bb1583b55751
rodolfowei <crm_b@hotmail.com>
2015-05-21 21:56:38 +0200

    Añadida la classe descriptors DB, para poder acceder a la base de datos

---
+++ src/comparador/descriptorsDB.java
@@ -0,0 +1,20 @@
+package comparador;
+
+import java.beans.Statement;
+import java.sql.Connection;
+
+public class descriptorsDB {
+	
+	Connection conexionDB;
+	Statement st;
+	
+	public de
+	
+	
+	
+	
+	
+	
+	
+
+}
